007aa895790eea71133f00ec7be7317e9942d9ce
Romain FERNANDEZ <rofernan@e2r10p12.42.fr>
2020-02-10 16:23:20 +0100

    update .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,14 +6,14 @@
 /*   By: rofernan <rofernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 10:40:51 by rofernan          #+#    #+#             */
-/*   Updated: 2020/02/10 14:52:23 by rofernan         ###   ########.fr       */
+/*   Updated: 2020/02/10 16:02:27 by rofernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "./libft/libft.h"
+# include "../libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
@@ -24,11 +24,16 @@
 # include <stdio.h>
 # include <sys/errno.h>
 
-typedef struct	s_list
+typedef struct	s_env
 {
 	char		*name;
 	char		*data;
 	t_list		*next;
-}				t_list;
+}				t_env;
+
+typedef struct	s_shell
+{
+	char		*str;
+}				t_shell;
 
 #endif
