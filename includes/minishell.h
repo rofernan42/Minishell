39468436cc90dc2969af00de49d17c6bb8fb8548
Romain FERNANDEZ <rofernan@e2r10p12.42.fr>
2020-02-10 10:48:40 +0100

    first push

---
+++ includes/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rofernan <rofernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/10 10:40:51 by rofernan          #+#    #+#             */
+/*   Updated: 2020/02/10 10:47:24 by rofernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+# include <stdio.h>
+# include <sys/errno.h>
+
+#endif
