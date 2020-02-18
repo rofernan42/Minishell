2c3d72625f1ef2686271870a3230f7c45d6f40aa
Romain FERNANDEZ <rofernan@e3r5p9.42.fr>
2020-02-18 11:03:13 +0100

    add main.c

--- main.c
+++ main.c
@@ -1,32 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rofernan <rofernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 12:50:36 by rofernan          #+#    #+#             */
-/*   Updated: 2020/02/17 15:52:30 by rofernan         ###   ########.fr       */
+/*   Updated: 2020/02/18 11:02:42 by rofernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/minishell.h"
-
-void	ft_echo(int ac, char **av, t_shell *shell)
-{
-	int i;
-
-	i = 1;
-	shell->str = ft_strdup("");
-	while (i < ac)
-	{
-		shell->str = ft_strjoin_free(shell->str, av[i], 1);
-		if (i < ac - 1)
-			shell->str = ft_strjoin_free(shell->str, " ", 1);
-		i++;
-	}
-	ft_putendl_fd(shell->str, 1);
-}
 
 int		enter(char *str)
 {
@@ -50,18 +34,6 @@ int		count_args(char **args)
 	while (args[i])
 		i++;
 	return (i);
-}
-
-void	del_args(t_shell *shell)
-{
-	int i;
-
-	i = 0;
-	while (shell->args[i])
-		ft_strdel(&shell->args[i++]);
-	ft_strdel(&shell->args[i]);
-	free(shell->args);
-	shell->args = NULL;
 }
 
 void	layout_args(t_shell *shell)
@@ -103,9 +75,8 @@ int main(int ac, char **av, char **envp)
 	char	buffer[BUF_SIZE + 1];
 	int		ret;
 	int		i;
-	t_env	*env;
 
-	init_env(&env);
+	init_env(&shell.env);
 	shell.args = NULL;
 	shell.command = ft_strdup("");
 	ret = 0;
@@ -120,41 +91,7 @@ int main(int ac, char **av, char **envp)
 			shell.args = ft_split(shell.command, ' ');
 			shell.args[0] = ft_strlow(shell.args[0]);
 			layout_args(&shell);
-			i = count_args(shell.args);
-			open_fd(&shell);
-
-			shell.stdout_cpy = dup(1);
-			close(1);
-			dup2(shell.fd, 1);
-			if (!ft_strcmp(shell.args[0], "echo"))
-				ft_echo(i, shell.args, &shell);
-			else if (!ft_strncmp(shell.args[0], "cd", 2))
-				ft_cd(shell.args[1], env);
-			else if (!ft_strcmp(shell.args[0], "pwd"))
-				ft_pwd(env);
-			else if (!ft_strcmp(shell.args[0], "export"))
-				ft_export(&shell.args[1], env);
-			else if (!ft_strcmp(shell.args[0], "unset"))
-				ft_unset(&shell.args[1], env);
-			else if (!ft_strcmp(shell.args[0], "env"))
-				ft_env(env);
-			else if (!ft_strcmp(shell.args[0], "exit"))
-				ret = 0;
-			else
-			{
-				if (fork() == 0)
-					execve(shell.args[0], shell.args, envp);
-				else
-					wait(NULL);
-			}
-			close(shell.fd);
-			dup2(shell.stdout_cpy, 1);
-			close(shell.stdout_cpy);
-			
-			free(shell.command);
-			shell.command = ft_strdup("");
-			del_args(&shell);
-			ft_putstr_fd("\033[33mminishell$\033[0m ", 1);
+			ft_stdin(&shell);
 		}
 		if (ret == 0)
 		{
