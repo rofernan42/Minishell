4c49ed27df7cb4b5fb271f4a92e94451d3017220
Audric GAY <augay@e2r7p11.42.fr>
2020-02-26 16:53:46 +0100

    fini_soon

--- srcs/ft_stdin.c
+++ srcs/ft_stdin.c
@@ -57,7 +57,7 @@ int execute_cmd2(char **s, t_shell *shell)
 	// ft_p(s);
 	execve(s[0], s, NULL);
 	close_stdinout(shell);
-	dprintf(2,"RET = %i\n", ret);
+	// dprintf(2,"RET = %i\n", ret);
 	return (ret);
 }
 
@@ -65,8 +65,8 @@ int    exec_pipe2(t_shell *shell, int i)
 {
     int     pdes[2];
     int     status;
-    pid_t   child_right;
-    pid_t   child_left;
+    pid_t   child_right = 25;
+    pid_t   child_left = 25;
 	int f;
 
     pipe(pdes);
@@ -87,13 +87,13 @@ int    exec_pipe2(t_shell *shell, int i)
 	}
 	if (shell->args && !(child_left = fork()))
     {
-		dprintf(2, "FORK LEFT\n");
         close(pdes[0]);
         dup2(pdes[1], STDOUT_FILENO);
         exit(execute_cmd2(shell->args, shell));
     }
     if (!(child_right = fork()))
     {
+
 		if (shell->args)
 		{
       		close(pdes[1]);
@@ -101,28 +101,25 @@ int    exec_pipe2(t_shell *shell, int i)
 		}
         if (still(shell) == 1)
 		{
-			// printf("RESTE PIPE\n");
 			h_split(shell, &shell->next_args);
             exec_pipe2(shell, i+1);
 		}
 		else if (shell->next_args != NULL)
 		{
-					dprintf(2, "FORK LEFT\n");
 
             exit(execute_cmd2(shell->next_args, shell));
 		}
 	}
     close(pdes[1]);
-    close(pdes[0]);
-    waitpid(child_left, 0, 0);
-    waitpid(child_right, &status, WUNTRACED);
-	printf("g_sig=%i, ret=%i, WEX=%i\n",getpid(), status, WEXITSTATUS(status));
-
+    close(pdes[0]);	
+	status = 0;
+    waitpid(child_left, &status, 0);
+	status = 0;
+    waitpid(child_right, &status, 0);
 	if (i != 0)
     	exit(WEXITSTATUS(status));
 	else
 	{
-		// close_stdinout(shell);
 		return (status);
 	}
 	
@@ -141,22 +138,16 @@ void		ft_stdin(t_shell *shell, char **command)
 	h_split(shell, &command);
 	ft_free(&command);
 	ret = exec_pipe2(shell, 0);
-		printf("g_sig=%i, ret=%i BLOBLB\n",g_sig, ret);
+		// printf("g_sig=%i, ret=%i BLOBLB\n",g_sig, ret);
 	if (g_sig == 11)
 		ret = 130;
 	else if (g_sig == 8)
 		ret = 131;
 	else if (ret == 1)
 		ret = 1;
-	else if (WEXITSTATUS(ret) == 54)
-		ret = 127;
-	else if (WTERMSIG(ret) == 112)
-		ret = 130;
-	else if (WTERMSIG(ret) == 3)
-		ret = 131;
 	else
 		ret = WEXITSTATUS(ret);
 	status_res(shell, ret);
-	printf("g_sig=%i, ret=%i BLOBLB\n",g_sig, ret);
+	// printf("g_sig=%i, ret=%i BLOBLB\n",g_sig, ret);
 	g_sig = 0;
 }
